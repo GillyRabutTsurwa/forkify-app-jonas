d8a62c60f8e3b9990f27a3c34341227ff5e1a3d1
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-05-21 16:41:17 -0500

    made our first API calls using async/await & try/catch

---
+++ notes.md
@@ -0,0 +1,6 @@
+# Notes.
+## Part 2 - API Calls
+*   Re-learnt making API calls.
+*   We are using **axios** and not **fetch**
+*   We do not use a proxy
+*   Refreshed my memory on using promises, both using **.then()** and using the more favourable **async/await**. 
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,55 @@
-import string from "./models/Search";
-import { addition, multiplication, ID } from "./views/searchView";
-// changing name of the functions in the new file comme je veux
-import { addition as add, multiplication as multiply, ID as myID } from "./views/searchView";
-// how to import absolutely everything from searchview.js
-    // we save everything in an object
-    
-import * as searchView from "./views/searchView";
+import axios from "axios";
 
-console.log(string);
-console.log(`Using imported functions: ${addition(ID, 2)} and ${multiplication(3,5)}`);
-console.log(`Using imported functions2: ${add(myID, 2)} and ${multiply(3,5 )}`);
-console.log(`Using imported functions3: ${searchView.addition(myID, 2)} and ${searchView.multiplication(3,5 )}`);
+// NOTE: Refresher on using promises can be found here. Just open this block to look at the code
+{
+    // async function getResults(query) {
+    //     try {
+    //         const response = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+    //         const infoObj = {
+    //             apiInfo: response,
+    //             apiData: response.data,
+    //             apiRecipes: response.data.recipes
+    //         }
+    //         console.log(infoObj);
+    //     }
+    //     catch(err) {
+    //         console.log(`Error: ${err}`);
+    //     }
+    // }
+    
+
+    // function getResults2(query) {
+    //     const response = axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+    //     response.then((info) => {
+    //         const infoObj = {
+    //             apiInfo: info,
+    //             apiData: info.data,
+    //             apiRecipes: info.data.recipes
+    //         }
+    //         console.log(infoObj);
+    //     }).catch((err) => {
+    //         console.log("There was an error in the pinchi");
+    //     });
+    // }
+    
+    // getResults("pizza");
+    // getResults2("pizza");
+}
+
+async function getResults(query) {
+    try {
+        const response = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        const infoObj = {
+            apiInfo: response,
+            apiData: response.data,
+            apiRecipes: response.data.recipes
+        }
+        console.log(infoObj);
+    }
+    catch(err) {
+        console.log(`${err} ma pinchi`)
+    }
+    
+}
+
+getResults("pizza");
