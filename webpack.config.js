a35611475f116ea6ab9d8024aaeec15cecf52210
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-11-10 10:21:59 -0600

    it's been a min

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,33 @@ const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: ["babel-polyfill", "./src/js/index.js"],
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "./js/bundle.js"
-    },
-    devServer: {
-        contentBase: "dist"
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            filename: "index.html",
-            template: "./src/index.html"
-        })
+  entry: ["babel-polyfill", "./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "./js/bundle.js",
+  },
+  devServer: {
+    contentBase: "dist",
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    }
-}
+  },
+};
